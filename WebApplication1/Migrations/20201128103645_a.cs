78854c6e3d2930fa77b59d4abb1271a965630f5a
Jesse Pardoen <19009437@student.hhs.nl>
2020-11-28 11:38:22 +0100

    Add project files.

---
+++ WebApplication1/Migrations/20201128103645_a.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Voornaam = table.Column<string>(maxLength: 10, nullable: true),
+                    Achternaam = table.Column<string>(nullable: false),
+                    Huisnummer = table.Column<int>(nullable: false),
+                    Straatnaam = table.Column<string>(nullable: true),
+                    Postcode = table.Column<string>(maxLength: 6, nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    StudentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Student_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_StudentId",
+                table: "Student",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [StringLength(10)]
+        public string Voornaam { get; set; }
+        [Required]
+        public string Achternaam { get; set; }
+        [Range(1,600, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Huisnummer { get; set; }
+        [DataType(DataType.Text)]
+        public string Straatnaam { get; set; }
+        [StringLength(6, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Postcode { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+        public List<Student> Vrienden { get; set; }
+    }
+}
